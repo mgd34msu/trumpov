50e83cdb2e0e4c3713fe7f4c8755f4327ffa7c94
mgd34msu <mikedavis@Michaels-MacBook-Pro.local>
2016-04-15 00:10:57 -0500

    now using a parser instead of regex
    rewrote the script to use an html parser instead of regular expression.

--- carpaccio.py
+++ carpaccio.py
@@ -1,20 +1,37 @@
+from bs4 import BeautifulSoup as bs
+from bs4 import SoupStrainer as ss
 import requests
-import re
+import sys
 
-def find_carpaccio():
-    r = requests.get('http://www.teiteirobata.com/new-page-2/')
-    r_list = re.split(r'<div class="menu-selector">|</div> <!-- END .menu-wrapper -->', r.text)
-    menu_list = re.split(r'<div class="menu-section-header">', r_list[1])
-    del menu_list[0]
-    for i in range(len(menu_list)):
-        menu_list[i] = re.sub(r'  |\n', '', menu_list[i])
-    menu_list = list(filter(None, re.split('<div[^>]*>|<input[^>]*>|<\/div>', ''.join(menu_list))))
-    indices = [i for i, s in enumerate(menu_list) if 'carpaccio' in s]
+# request page, then load only the div tags with 'menu-item' class attribute into memory
+url = requests.get('http://www.teiteirobata.com/new-page-2/')
+items = bs(url.text, 'html.parser', parse_only=ss('div', class_='menu-item')).contents
+
+# create menu dictionary, then fill it with menu items and their descriptions
+menu = {}
+for x in range(len(items)):
+    # checks to see if a description is available for the menu item
+    # note: items with '3' as their length do not have descriptions
+    if len(items[x]) == 3:
+        item = str(items[x].contents[1].contents[0]).strip()
+        desc = ''
+    else:
+        item = str(items[x].contents[1].contents[0]).strip()
+        desc = str(items[x].contents[3].contents[0]).strip()
+    menu[item] = desc
+
+# function to perform a search on the menu dictionary
+# note:  default search is for carpaccio, because it is awesome.
+def find_in_menu(sub='carpaccio'):
     try:
-        carpaccio = [menu_list[indices[0]], menu_list[indices[0]+1]]
-        print('Today\'s Special: ' + carpaccio[0].title() + " -- " + carpaccio[1])
+        search = [s for s in menu if sub in s]
+        result_item, result_desc = str(search[0]), menu[str(search[0])]
+        print(result_item.title() + '\n' + result_desc)
     except:
-        print('Sorry, not on the menu today...')
+        print('Sorry, \"' + str(sub) + '\" is not on the menu today.')
 
 if __name__ == '__main__':
-    find_carpaccio()
+    if len(sys.argv) > 1:
+        find_in_menu(str(sys.argv[1]))
+    else:
+        find_in_menu()
