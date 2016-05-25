baf320442b48d0f23deab44c8b986a314a0a0a9c
mgd34msu <mgd34msu@gmail.com>
2016-05-25 01:48:11 -0500

    added regression.py; updated .gitignore
    machine learning - regression analysis using Quandl

---
+++ regression.py
@@ -0,0 +1,61 @@
+from matplotlib import style
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import datetime
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import Quandl
+
+
+style.use('ggplot')
+
+df = Quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+df.fillna(-99999, inplace=True)
+
+forecast_col = 'Adj. Close'
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+# label column will be Adj. Close, 3 days into the future
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+# separate features from label, set as x & y
+x = np.array(df.drop(['label'], 1))
+x = preprocessing.scale(x)
+x_lately = x[-forecast_out:]
+x = x[:-forecast_out]
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=0.2)
+
+clf = LinearRegression()
+# clf = svm.SVR()
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+
+forecast_set = clf.predict(x_lately)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
