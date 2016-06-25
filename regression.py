fce723a20ff6c83afa04c194d03ae18d19d97491
mgd34msu <mgd34msu@gmail.com>
2016-06-24 23:30:04 -0500

    updated regression w/ pickle -- also updated gitignore

--- regression.py
+++ regression.py
@@ -1,5 +1,5 @@
 from matplotlib import style
-from sklearn import preprocessing, cross_validation, svm
+from sklearn import preprocessing, cross_validation
 from sklearn.linear_model import LinearRegression
 import datetime
 import math
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import Quandl
-
+import pickle
 
 style.use('ggplot')
 
@@ -34,11 +34,16 @@ y = np.array(df['label'])
 
 x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=0.2)
 
-clf = LinearRegression()
-# clf = svm.SVR()
+clf = LinearRegression(n_jobs=-1)
 clf.fit(x_train, y_train)
-accuracy = clf.score(x_test, y_test)
 
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+accuracy = clf.score(x_test, y_test)
 forecast_set = clf.predict(x_lately)
 
 df['Forecast'] = np.nan
@@ -52,6 +57,8 @@ for i in forecast_set:
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+print(df.tail())
 
 df['Adj. Close'].plot()
 df['Forecast'].plot()
