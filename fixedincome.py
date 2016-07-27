1c7266555c49b7d00897b3157d4e675048ad8a58
mgd34msu <mgd34msu@gmail.com>
2016-07-27 16:31:02 -0500

    more updates to fixed income maths

--- fixedincome.py
+++ fixedincome.py
@@ -78,10 +78,14 @@ def periodic_rate(y, m):
 # 2(a) The Price of a Bond
 
 
+def coupon(par, coup_rate, m=2):
+    return par * (coup_rate / m)
+
+
 def bond_price(par, coup_rate, req_rate, n, m=2):
     """ price of a bond, given its par value, coupon rate, required rate of return, and tenor """
     """ assumes semi-annual payments, but can be changed """
-    c = par * coup_rate / m
+    c = coupon(par, coup_rate, m)
     return pv_annu(c, req_rate, n, m) + pv(par, req_rate, n, m)
 
 
@@ -97,7 +101,7 @@ def yield_maturity(price, par, cr, rr, n, m=2, accuracy=50):
     """ finds YTM by trial and error, within the bounds of price plus/minus an accuracy factor, default at 50 """
     bp_est = bond_price(par, cr, rr, n, m)
     if price - accuracy <= bp_est <= price + accuracy:
-        return round(rr, 3)
+        return round(rr, 5)
     elif bp_est < price - accuracy:
         return yield_maturity(price, par, cr, rr - .005, n, m, accuracy)
     else:
@@ -120,4 +124,53 @@ def port_yield_wa(*args):
 
 
 # 2(c) Potential Sources of Dollar Return
+
+
+def ci_plus_ioi(par, coup_rate, reinv_rate, n, m=2):
+    """ coupon interest plus interest on interest """
+    return (coupon(par, coup_rate, m)) * ((((1 + (reinv_rate / m)) ** (n * m)) - 1) / (reinv_rate / m))
+
+
+def int_on_int(par, coup_rate, reinv_rate, n, m=2):
+    """ interest on interest """
+    return ci_plus_ioi(par, coup_rate, reinv_rate, n, m) - (n * m * coupon(par, coup_rate, m))
+
+
+def ioi_pdr(par, coup_rate, reinv_rate, n, m=2):
+    """ interest on interest as a percentage of total dollar return """
+    return round(int_on_int(par, coup_rate, reinv_rate, n, m) / ci_plus_ioi(par, coup_rate, reinv_rate, n, m), 4)
+
+
+# 2(d) Total Return
+
+
+def total_return(par, coup_rate, reinv_rate, n, price=1000, m=2):
+    """ total return on a bond held to maturity """
+    future_amt = ci_plus_ioi(par, coup_rate, reinv_rate, n, m) + par
+    return_pct = (((future_amt / price) ** (1 / (n * m))) - 1) * m
+    return future_amt, round(return_pct, 4)
+
+
+def total_return_limit(par, coup_rate, reinv_rate, ytm_rate, n_full, n_used, price=1000, m=2):
+    """ total return on a bond sold prior to maturity """
+    ci_and_ioi = ci_plus_ioi(par, coup_rate, reinv_rate, n_used, m)
+    pv_coup_int = pv_annu(coupon(par, coup_rate, m), ytm_rate, n_full - n_used, m)
+    pv_maturity = pv(par, ytm_rate, n_full - n_used, m)
+    future_amt = ci_and_ioi + pv_coup_int + pv_maturity
+    return_pct = (((future_amt / price) ** (1 / (n_used * m))) - 1) * m
+    return future_amt, round(return_pct, 4)
+
+
+# 3. Bond Price Volatility
+
+# 3(a) Price Volatility of Option-Free Bonds
+
+def bp_vol(par, coup_rate, req_rate, yield_bps_chg, n, m=2):
+    """ volatility of bond price, given +/- bps change in required yield """
+    new = bond_price(par, coup_rate, req_rate + (yield_bps_chg / 10000), n, m)
+    old = bond_price(par, coup_rate, req_rate, n, m)
+    return round((new - old) / old, 4)
+
+
+# 3(b) Price Volatility Measures:  PVBP and YV of a Price Change
 
